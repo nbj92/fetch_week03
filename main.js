33a930cb81395d9ebc6775718001d4df6262d8ea
Enebye91 <eaamivj@students.eaaa.dk>
2023-09-11 12:32:59 +0200

    fetch response

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-const response = await fetch(``)
+const response = await fetch(`https://wu-e23a-fetch-arrays-default-rtdb.europe-west1.firebasedatabase.app/pua-pizzaer.json`)
+.then(function (response) {
+    return response.json();
+})
+
+.then(function (pizza) {
+    pizza.forEach(function (pizza) {
+        console.log(pizza); 
+    });
+})
+.catch(function (error) {
+    console.log.error(error);
+}); 
