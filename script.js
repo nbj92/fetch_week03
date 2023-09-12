730bcfd49a531f3357580e34bfbc377eedf66083
Nicolai Jensen <nbj_92@hotmail.com>
2023-09-12 13:57:11 +0200

    Data kan hentes og indsættes i index fra script

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+// POST DATA TIL FIREBASE
+
 // const pizzaer =
 // [
 //   {
 //     name: "Hawaii",
-//     toppings: ["ananans", "skinke"]
+//     toppings: ["ananas", "skinke"]
 //   },
 //   {
 //     name: "Genova",
-//     toppings: ["ananans", "kylling", "karry"]
+//     toppings: ["ananas", "kylling", "karry"]
 //   },
 //   {
 //     name: "Tango",
@@ -17,6 +19,7 @@
 //     toppings: ["oksekød", "spinat", "champignon", "rødløg"]
 //   }
 // ];
+
 
 // async function postData(url = "", data = {}) {
 //     // Default options are marked with *
@@ -29,66 +32,87 @@
       
 //       body: JSON.stringify(data), // body data type must match "Content-Type" header
 //     });
-//     return response.json(); // parses JSON response into native JavaScript objects
+//     // return response.json(); // parses JSON response into native JavaScript objects
 //   }
 
 //   postData("https://wu-e23a-fetch-arrays-default-rtdb.europe-west1.firebasedatabase.app/pua-pizzaer.json", pizzaer);
 
-function udenTopping(pizzaer, tp) {
-    console.log("kører");
-    console.log(pizzaer);
-    const result = pizzaer.filter(p => {
-        p.topping["toppings"].forEach(t => {
-            if(t==tp) {
-                console.log(t);
-                return true;
-            }     
-        });
-        return false;
-      });
-      console.log("kører endnu");
 
-      return result;
-} 
+// GET DATA; MANIPULER OG INDSÆT I index.php
 
-async function getData() {
+const list = document.querySelector(".menu_indhold div");
+// console.log(list);
+
+// Kalder udenTopping og mapper array'et således at et nyt array indeholder list items <li>
+// ad hvert element i array'et. Disse list items tilføjes til div elementet der indholder listen
+// i html dokumentet.
+const menukort = async (topping) => {
     try {
-    // const response = await fetch("https://wu-e23a-fetch-arrays-default-rtdb.europe-west1.firebasedatabase.app/pua-pizzaer.json");
-        const response = await fetch("pizza.json");
-        const pizza = await response.json();
-    // console.log(pizza);
-    // console.log(Object.keys(pizza));
-    // console.log(pizza[Object.keys(pizza)]);
-    // const data = pizza[Object.keys(pizza)];
-    // console.log(data[0]);
-        console.log(pizza);
-        
+        list.innerHTML ="<ul>";
+    const menukortet = await udenTopping(topping);
+    
+    let piz = menukortet.map(e => { 
+        return "<li>"+e.name+"</li>";
+    }).forEach(e => list.innerHTML += e);
 
-
-
-    } catch(err) {
+        list.innerHTML += "</ul>";
+    } catch (err) {
         console.error(err);
     }
-    // const data = pizza[Object.keys(pizza)];
-    // for(let pizza of data) {
-    //     console.log(pizza["name"]);
-    // }
-  }
+};
 
-  const pizzaer = getData();
+// kalder getData() for at hente data, hvorefter der filtreres så der returneres et array
+// der IKKE indeholder den "topping" der er angivet i parameteren
+async function udenTopping(topping) {
+    try {
+        let pizzaer = await getData();
+        // console.log(pizzaer);
+        // console.log(pizzaer[0]["toppings"]);
+        const result = pizzaer.filter(p => {
+            p["toppings"].forEach(t => {
+                if(t===topping) {
+                // console.log(p["name"]);
+                // console.log(t);
+                return false;
+                }     
+            });
+            return true;
+        });
+        return result;
 
+    } catch (err) {
+        console.error(err);
+    }
+
+    
+} 
+
+
+// Henter data fra vores kilde: her er det pizza.json. Der er udkommenteret firebase source
+// Data returneres som et array
+async function getData() {
+    try {
+        // const response = await fetch("https://wu-e23a-fetch-arrays-default-rtdb.europe-west1.firebasedatabase.app/pua-pizzaer.json");
+        const response = await fetch("pizza.json");
+        const pizza = await response.json();
+        return pizza;
+    } catch(err) {
+        console.error(err);
+    }    
+}
   
+async function getpi() {
+    try {
+        await getData();
+        console.log(pizzaer);
+    } catch (err) {
+        console.error(err);
+    }
 
-//   udenTopping(pizzaer, "ananas");
+}
+// getData();
+// getpi();
 
-//   console.log(udenTopping(pizzaer, "ananas"));
-
-//   const result = pizzaer.filter(p, t => {
-//     p.topping["toppings"].forEach(topping => {
-//         if(topping==t) {
-//             return true;
-//         }     
-//     });
-//     return false;
-//   });
+// udenTopping("ananas");
+menukort("ananas");
 
